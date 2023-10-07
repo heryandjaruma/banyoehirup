1c26b08d774cb52ff987746f813e6001af92b940
nayr <heryandjaruma@gmail.com>
2023-10-07 11:23:51 +0700

    set figma screen to device height

--- src/app/layout/Landing.tsx
+++ src/app/layout/Landing.tsx
@@ -64,7 +64,7 @@ export default function Landing() {
 
       {isLandingVisible && (
         <div
-          className={`w-screen h-screen grid place-items-center ${fira_code.className}`}
+          className={`h-screen grid place-items-center ${fira_code.className} aspect-video`}
           onClick={handleLandingClick}
         >
           <div className="">
